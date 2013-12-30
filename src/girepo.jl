d69cdc5047cca34dbeb2538b7c49c871dac2b964
Björn Linse <bjorn.linse@gmail.com>
2013-12-30 17:21:42 +0100

    add GINamespace, access methdods

--- src/girepo.jl
+++ src/girepo.jl
@@ -1,4 +1,6 @@
 @GType GIRepository
+const girepo = GIRepository(ccall((:g_irepository_get_default, libgi), Ptr{GObject}, () ))
+
 abstract GITypelib
 
 abstract GIBaseInfo
@@ -10,13 +12,13 @@ type GIInfo#{Name}
             error("Cannot constrct GIInfo from NULL")
         end
         info = new(h)
-        finalizer(info, free_info)
+        finalizer(info, info_unref)
         info
     end
 end
 
 # don't call directly, called by gc
-function free_info(info::GIInfo) 
+function info_unref(info::GIInfo) 
     ccall((:g_base_info_unref, libgi), Void, (Ptr{GIBaseInfo},), info.handle)
     info.handle = C_NULL
 end
@@ -37,13 +39,22 @@ function show(io::IO, info::GIInfo)
     print(io, "GIInfo(:", info_get_namespace(info), ", :", info_get_name(info), ")")
 end
 
+immutable GINamespace
+    name::Symbol
+    function GINamespace(namespace::Symbol, version=nothing)
+        #TODO: stricter version sematics?
+        gi_require(namespace, version)
+        new(namespace)
+    end
+end 
+convert(::Type{Symbol}, ns::GINamespace) = ns.name
+convert(::Type{Ptr{Uint8}}, ns::GINamespace) = convert(Ptr{Uint8}, ns.name)
 
-#TODO: display(info)
-
-const girepo = GIRepository(ccall((:g_irepository_get_default, libgi), Ptr{GObject}, () ))
-
-function gi_require(namespace::Symbol, version=C_NULL,)
-    local typelib::Ptr{GITypelib}
+function gi_require(namespace, version=nothing)
+    #local typelib::Ptr{GITypelib}
+    if version==nothing
+        version = C_NULL
+    end
     GError() do error_check
         typelib = ccall((:g_irepository_require, libgi), Ptr{GITypelib}, 
             (Ptr{GObject}, Ptr{Uint8}, Ptr{Uint8}, Cint, Ptr{Ptr{GError}}), 
@@ -54,26 +65,26 @@ function gi_require(namespace::Symbol, version=C_NULL,)
     #return GITypelib(typelib)
 end
 
-
-function gi_find_by_name(namespace::Symbol, name::Symbol)
+function gi_find_by_name(namespace, name)
     info = ccall((:g_irepository_find_by_name, libgi), Ptr{GIBaseInfo}, 
            (Ptr{GObject}, Ptr{Uint8}, Ptr{Uint8}), girepo, namespace, name)
     if info == C_NULL
         error("Name $name not found in $namespace")
     end
     GIInfo(info) #TODO: Infer type
-    #return GITypelib(typelib)
 end
 
-GIInfo(namespace::Symbol, name::Symbol) = gi_find_by_name(namespace, name)
+GIInfo(namespace, name::Symbol) = gi_find_by_name(namespace, name)
 
-#TODO: make iterator
+#TODO: make ns behave more like Array and/or Dict{Symbol,GIInfo}?
+length(ns::GINamespace) = int(ccall((:g_irepository_get_n_infos, libgi), Cint, (Ptr{GObject}, Ptr{Uint8}), girepo, ns))
+function getindex(ns::GINamespace, i::Integer) 
+    GIInfo(ccall((:g_irepository_get_info, libgi), Ptr{GIBaseInfo}, (Ptr{GObject}, Ptr{Uint8}, Cint), girepo, ns, i ))
+end
+function getindex(ns::GINamespace, name::Symbol) 
+    gi_find_by_name(ns, name)
+end
+
 function gi_get_infos(namespace::Symbol) 
-    n_entries = ccall((:g_irepository_get_n_infos, libgi), Cint, (Ptr{GObject}, Ptr{Uint8}), girepo, namespace)
-    if n_entries <= 0
-        error("no names found in $namespace")
-    end
-    [GIInfo(ccall((:g_irepository_get_info, libgi), Ptr{GIBaseInfo}, (Ptr{GObject}, Ptr{Uint8}, Cint), girepo, namespace, n )) for n in 0:n_entries-1]
+    [ns[i] for i=length(ns)]
 end
-
-
