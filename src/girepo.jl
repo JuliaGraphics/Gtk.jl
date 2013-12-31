64b58ddac62b86e4377c06943bdd8258f11ea58a
Björn Linse <bjorn.linse@gmail.com>
2013-12-31 14:06:40 +0100

    extract types of arguments (+ cleanup)

--- src/girepo.jl
+++ src/girepo.jl
@@ -13,7 +13,7 @@ function GIInfo(h::Ptr{GIBaseInfo})
     if h == C_NULL 
         error("Cannot constrct GIInfo from NULL")
     end
-    typeid = int(ccall((:g_base_info_get_type, libgi), Cint, (Ptr{GIBaseInfo},), h))
+    typeid = int(ccall((:g_base_info_get_type, libgi), Enum, (Ptr{GIBaseInfo},), h))
     info = GIInfo{typeid}(h)
     finalizer(info, info_unref)
     info
@@ -26,15 +26,15 @@ end
 
 convert(::Type{Ptr{GIBaseInfo}},w::GIInfo) = w.handle
 
-const GIInfoTypes = (:Invalid, :Function, :Callback, :Struct, :Boxed, :Enum, :Flags, :Object, :Interface, :Constant, :Unknown, :Union, :Value, :Signal, :VFunc, :Property, :Field, :Arg, :Type, :Unresolved)
-const GIInfoTypeNames = [ Base.symbol("GI$(name)Info") for name in GIInfoTypes]
-baremodule GIInfoType 
-end    
+const GIInfoTypesShortNames = (:Invalid, :Function, :Callback, :Struct, :Boxed, :Enum, :Flags, :Object, :Interface, :Constant, :Unknown, :Union, :Value, :Signal, :VFunc, :Property, :Field, :Arg, :Type, :Unresolved)
+const GIInfoTypeNames = [ Base.symbol("GI$(name)Info") for name in GIInfoTypesShortNames]
 
-for (i,itype) in enumerate(GIInfoTypes)
-    let uppername = symbol(uppercase(string(itype)))
-        eval(GIInfoType,:(const $uppername = $(i-1)))
+const GIInfoTypes = Dict{Symbol, Type}()
+
+for (i,itype) in enumerate(GIInfoTypesShortNames)
+    let lowername = symbol(lowercase(string(itype)))
         @eval typealias $(GIInfoTypeNames[i]) GIInfo{$(i-1)}
+        GIInfoTypes[lowername] = GIInfo{i-1}
     end
 end
 
@@ -45,6 +45,9 @@ function get_name(info::GIInfo)
     str = ccall((:g_base_info_get_name, libgi), Ptr{Uint8}, (Ptr{GIBaseInfo},), info)
     symbol(bytestring(str)) 
 end
+
+#Bug in gobject
+get_name(info::GITypeInfo) = symbol("<gtype>")
 
 function get_namespace(info::GIInfo) 
     str = ccall((:g_base_info_get_namespace, libgi), Ptr{Uint8}, (Ptr{GIBaseInfo},), info)
@@ -102,22 +105,74 @@ function getindex(ns::GINamespace, name::Symbol)
     gi_find_by_name(ns, name)
 end
 
-# Registered types
-const _typemap = [
-    :callable => GICallableInfo, 
-    :method => GIFunctionInfo, 
-    :signal => GISignalInfo,
-    :vfunc => GIVFuncInfo,
-    :object => GIObjectInfo,
-    :interface => GIInterfaceInfo,
-    :arg => GIArgInfo
-] 
+GIInfoTypes[:method] = GIFunctionInfo
+GIInfoTypes[:callable] = GICallableInfo
 
+# one-> many relationships
 for (owner, property) in [
     (:object, :method), (:object, :signal), (:object, :interface),
+    (:object, :property), (:object, :constant), (:object, :field),
     (:interface, :method), (:interface, :signal), (:callable, :arg)]
-    @eval function $(symbol("get_$(property)s"))(info::$(_typemap[owner]))
+    @eval function $(symbol("get_$(property)s"))(info::$(GIInfoTypes[owner]))
         n = int(ccall(($("g_$(owner)_info_get_n_$(property)s"), libgi), Cint, (Ptr{GIBaseInfo},), info))
-        $(_typemap[property])[ GIInfo( ccall(($("g_$(owner)_info_get_$property"), libgi), Ptr{GIBaseInfo}, (Ptr{GIBaseInfo}, Cint), info, i)) for i=0:n-1]
+        $(GIInfoTypes[property])[ GIInfo( ccall(($("g_$(owner)_info_get_$property"), libgi), Ptr{GIBaseInfo}, (Ptr{GIBaseInfo}, Cint), info, i)) for i=0:n-1]
     end
+    if property == :method
+        @eval function $(symbol("find_$(property)"))(info::$(GIInfoTypes[owner]), name)
+            GIInfo(ccall(($("g_$(owner)_info_find_$(property)"), libgi), Ptr{GIBaseInfo}, (Ptr{GIBaseInfo},Ptr{Uint8}), info, name))
+        end
+    end
+end
+
+# one->one
+_unit(x) = x
+_types = [GIInfo=>(Ptr{GIBaseInfo},GIInfo)]
+for (owner,property,typ) in [
+    (:callable, :return_type, GIInfo), (:callable, :caller_owns, Enum),
+    (:arg, :type, GIInfo), (:arg, :direction, Enum),
+    (:type, :tag, Enum), (:type, :interface, GIInfo)]
+    ctype, conv = get(_types, typ, (typ,_unit))
+    @eval function $(symbol("get_$(property)"))(info::$(GIInfoTypes[owner]))
+        $conv(ccall(($("g_$(owner)_info_get_$(property)"), libgi), $ctype, (Ptr{GIBaseInfo},), info))
+    end
+end
+
+for (owner,flag) in [ (:type, :is_pointer) ]
+    @eval function $flag(info::$(GIInfoTypes[owner]))
+        ret = ccall(($("g_$(owner)_info_$(flag)"), libgi), Cint, (Ptr{GIBaseInfo},), info)
+        return ret != 0
+    end
+end
+
+
+const typetag_primitive = [
+    Void,Bool,Int8,Uint8,
+    Int16,Uint16,Int32,Uint32,
+    Int64,Uint64,Cfloat,Cdouble,
+    Csize_t, # FIXME: Gtype
+    UTF8String
+    ]
+const TAG_BASIC_MAX = 13
+const TAG_INTERFACE = 16 
+
+extract_type(info::GIArgInfo) = extract_type(get_type(info))
+
+function extract_type(info::GITypeInfo)
+    tag = get_tag(info)
+    if tag <= TAG_BASIC_MAX
+        basetype = typetag_primitive[tag-1]
+    elseif tag == TAG_INTERFACE
+        # Object Types n such
+        iface = get_interface(info)
+        basetype = extract_type(iface)
+    end
+    if is_pointer(info)
+        Ptr{basetype}
+    else
+        basetype
+    end
+end
+
+function extract_type(info::GIObjectInfo) 
+    GObjectI # TODO: specialize
 end
--- test/girepo_test.jl
+++ test/girepo_test.jl
@@ -3,15 +3,13 @@ using Gtk;
 gtk = Gtk.GINamespace(:Gtk)
 
 window = gtk[:Window]
-
 @assert isa(window, Gtk.GIObjectInfo)
 
-meths = Gtk.get_methods(window)
-
-mmove =  meths[find([Gtk.get_name(m) == :move for m in meths])]
-@assert length(mmove) == 1
-move = mmove[1]
+move = Gtk.find_method(window,:move)
 
 args = Gtk.get_args(move)
-
 @assert length(args) == 2
+
+argx = args[1]
+@assert Gtk.get_name(argx) == :x
+@assert Gtk.extract_type(argx) == Int16 
