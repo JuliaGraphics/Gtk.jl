d6210eb326170bea377b37e2f8d4b97b83839e4b
Isaiah Norton <isaiah.norton@gmail.com>
2014-07-23 23:59:36 -0400

    fix bindeps install on linux

--- deps/build.jl
+++ deps/build.jl
@@ -3,12 +3,14 @@ using BinDeps
 @BinDeps.setup
 
 deps = [
-    gtk = library_dependency("gtk", aliases = ["libgtk-3-0"])
-    gdk = library_dependency("gdk", aliases = ["libgdk-3-0"])
-    gdk_pixbuf = library_dependency("gdk_pixbuf", aliases = ["libgdk_pixbuf-2.0-0"])
-    glib = library_dependency("glib", aliases = ["libglib-2.0-0"], os = :Windows)
-    gobject = library_dependency("gobject", aliases = ["libgobject-2.0-0", "libgobject-2.0", "libgobject-2_0-0"], os = :Windows)
-    gio = library_dependency("gio", aliases = ["libgio-2.0-0"])
+    gtk = library_dependency("gtk", aliases = ["libgtk-3", "libgtk-3-0"])
+    @windows_only begin
+        gdk = library_dependency("gdk", aliases = ["libgdk-3-0"])
+        gdk_pixbuf = library_dependency("gdk_pixbuf", aliases = ["libgdk_pixbuf-2.0-0"])
+        glib = library_dependency("glib", aliases = ["libglib-2.0-0"], os = :Windows)
+        gobject = library_dependency("gobject", aliases = ["libgobject-2.0-0", "libgobject-2.0", "libgobject-2_0-0"], os = :Windows)
+        gio = library_dependency("gio", aliases = ["libgio-2.0-0"])
+    end
 ]
 
 @linux_only begin
@@ -22,7 +24,8 @@ end
     provides(WinRPM.RPM,"libgobject-2_0-0", [gobject], os = :Windows)
 end
 
-@BinDeps.install @windows_only [
+if OS_NAME == :Windows
+@BinDeps.install [
     :gtk => :libgtk,
     :gdk => :libgdk,
     :gdk_pixbuf => :libgdk_pixbuf,
@@ -30,3 +33,6 @@ end
     :gobject => :libgobject,
     :glib => :libglib
 ]
+else
+@BinDeps.install
+end
