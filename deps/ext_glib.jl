b3d2af5d0f3319a3afe965c5c3f90699c6a303f2
Elliot Saba <staticfloat@gmail.com>
2014-07-29 17:44:54 -0400

    Move XDG stuff from deps/build.jl to deps/ext_glib.jl

--- deps/ext_glib.jl
+++ deps/ext_glib.jl
@@ -11,3 +11,17 @@ else
         const libglib = "libglib-2.0"
     end
 end
+
+@osx_only begin
+    if Pkg.installed("Homebrew") != nothing
+        using Homebrew
+        if Homebrew.installed("gtk+3")
+            # Append to XDG_DATA_DIRS to get us the proper paths setup for glib schemas
+            if "XDG_DATA_DIRS" in ENV
+                ENV["XDG_DATA_DIRS"] *= ":" * joinpath(Homebrew.brew_prefix, "share")
+            else
+                ENV["XDG_DATA_DIRS"] = joinpath(Homebrew.brew_prefix, "share")
+            end
+        end
+    end
+end
