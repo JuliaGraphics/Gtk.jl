3d5ddb7c80d40b18f76bab32695270eb65be8179
Elliot Saba <staticfloat@gmail.com>
2014-08-06 17:17:32 -0400

    * Update to use new BinDeps features, namely library groups and onload hooks! * Add a .gitignore to never commit deps/deps.jl, along with those pesky .DS_Store files
    (cherry picked from commit 888cb58af15364734e21b0cc8ce78babc9361387)

--- deps/build.jl
+++ deps/build.jl
@@ -2,15 +2,15 @@ using BinDeps
 
 @BinDeps.setup
 
+group = library_group("gtk")
+
 deps = [
-    gtk = library_dependency("gtk", aliases = ["libgtk-3", "libgtk-3-0"])
-    @windows_only begin
-        gdk = library_dependency("gdk", aliases = ["libgdk-3-0"])
-        gdk_pixbuf = library_dependency("gdk_pixbuf", aliases = ["libgdk_pixbuf-2.0-0"])
-        glib = library_dependency("glib", aliases = ["libglib-2.0-0"], os = :Windows)
-        gobject = library_dependency("gobject", aliases = ["libgobject-2.0-0", "libgobject-2.0", "libgobject-2_0-0"], os = :Windows)
-        gio = library_dependency("gio", aliases = ["libgio-2.0-0"])
-    end
+    gtk = library_dependency("gtk", aliases = ["libgtk-3", "libgtk-3-0"], group = group)
+    glib = library_dependency("glib", aliases = ["libglib-2.0", "libglib-2.0-0"], group = group)
+    gdk = library_dependency("gdk", aliases = ["libgdk-3-0", "libgdk-3.0"], group = group)
+    gdk_pixbuf = library_dependency("gdk_pixbuf", aliases = ["libgdk_pixbuf-2.0-0", "libgdk_pixbuf-2.0.0"], group = group)
+    gio = library_dependency("gio", aliases = ["libgio-2.0-0","libgio-2.0.0"], group = group)
+    gobject = library_dependency("gobject", aliases = ["libgobject-2.0.0", "libgobject-2.0-0", "libgobject-2.0", "libgobject-2_0-0"], group = group)
 ]
 
 @linux_only begin
@@ -20,16 +20,26 @@ end
 
 @windows_only begin
     using WinRPM
-    provides(WinRPM.RPM,"gtk3", [gtk,gdk,gdk_pixbuf,glib,gio], os = :Windows)
+    provides(WinRPM.RPM,"gtk3", [gtk,gdk,gdk_pixbuf,glib,gio], os = :Windows )
     provides(WinRPM.RPM,"libgobject-2_0-0", [gobject], os = :Windows)
 end
 
 @osx_only begin
     using Homebrew
-    provides(Homebrew.HB, "gtk+3", gtk, os = :Darwin)
+    provides(Homebrew.HB, "gtk+3", [gtk, gdk, gobject], os = :Darwin, onload = 
+    """
+    function __init__()
+        if "XDG_DATA_DIRS" in ENV
+            ENV["XDG_DATA_DIRS"] *= ":" * joinpath("$(Homebrew.brew_prefix)", "share")
+        else
+            ENV["XDG_DATA_DIRS"] = joinpath("$(Homebrew.brew_prefix)", "share")
+        end
+    end
+    """ )
+    provides(Homebrew.HB, "glib", [glib, gio], os = :Darwin)
+    provides(Homebrew.HB, "gdk-pixbuf", gdk_pixbuf, os = :Darwin)
 end
 
-if OS_NAME == :Windows
 @BinDeps.install [
     :gtk => :libgtk,
     :gdk => :libgdk,
@@ -38,6 +48,3 @@ if OS_NAME == :Windows
     :gobject => :libgobject,
     :glib => :libglib
 ]
-else
-@BinDeps.install
-end
