36746a4a7e76771f33781e6452ce1a803d982e0a
Jameson Nash <vtjnash@gmail.com>
2014-09-17 00:40:51 -0400

    unbreak linux build and cleanup library_dependency aliases to always list [unix-name, windows-name]

--- deps/build.jl
+++ deps/build.jl
@@ -5,17 +5,17 @@ using BinDeps
 group = library_group("gtk")
 
 deps = [
-    gtk = library_dependency("gtk", aliases = ["libgtk-3", "libgtk-3-0"], group = group)
     glib = library_dependency("glib", aliases = ["libglib-2.0", "libglib-2.0-0"], group = group)
-    gdk = library_dependency("gdk", aliases = ["libgdk-3-0", "libgdk-3.0"], group = group)
-    gdk_pixbuf = library_dependency("gdk_pixbuf", aliases = ["libgdk_pixbuf-2.0-0", "libgdk_pixbuf-2.0.0"], group = group)
-    gio = library_dependency("gio", aliases = ["libgio-2.0-0","libgio-2.0.0"], group = group)
-    gobject = library_dependency("gobject", aliases = ["libgobject-2.0.0", "libgobject-2.0-0", "libgobject-2.0", "libgobject-2_0-0"], group = group)
+    gobject = library_dependency("gobject", aliases = ["libgobject-2.0", "libgobject-2.0-0"], group = group)
+    gtk = library_dependency("gtk", aliases = ["libgtk-3", "libgtk-3-0"], group = group)
+    gdk = library_dependency("gdk", aliases = ["libgdk-3", "libgdk-3-0"], group = group)
+    gdk_pixbuf = library_dependency("gdk_pixbuf", aliases = ["libgdk_pixbuf-2.0", "libgdk_pixbuf-2.0-0"], group = group)
+    gio = library_dependency("gio", aliases = ["libgio-2.0", "libgio-2.0-0"], group = group)
 ]
 
 @linux_only begin
-    provides(AptGet, "libgtk-3-0", gtk)
-    provides(Yum, "gtk3", gtk)
+    provides(AptGet, "libgtk-3-0", deps)
+    provides(Yum, "gtk3", deps)
 end
 
 @windows_only begin
@@ -27,24 +27,24 @@ end
 @osx_only begin
     using Homebrew
     provides(Homebrew.HB, "gtk+3", [gtk, gdk, gobject], os = :Darwin, onload =
-    """
-    function __init__bindeps__()
-        if "XDG_DATA_DIRS" in ENV
-            ENV["XDG_DATA_DIRS"] *= ":" * joinpath("$(Homebrew.brew_prefix)", "share")
-        else
-            ENV["XDG_DATA_DIRS"] = joinpath("$(Homebrew.brew_prefix)", "share")
+        """
+        function __init__bindeps__()
+            if "XDG_DATA_DIRS" in ENV
+                ENV["XDG_DATA_DIRS"] *= ":" * joinpath("$(Homebrew.brew_prefix)", "share")
+            else
+                ENV["XDG_DATA_DIRS"] = joinpath("$(Homebrew.brew_prefix)", "share")
+            end
         end
-    end
-    """)
+        """)
     provides(Homebrew.HB, "glib", [glib, gio], os = :Darwin)
     provides(Homebrew.HB, "gdk-pixbuf", gdk_pixbuf, os = :Darwin)
 end
 
 @BinDeps.install [
+    :glib => :libglib,
+    :gobject => :libgobject,
     :gtk => :libgtk,
     :gdk => :libgdk,
     :gdk_pixbuf => :libgdk_pixbuf,
     :gio => :libgio,
-    :gobject => :libgobject,
-    :glib => :libglib
 ]
