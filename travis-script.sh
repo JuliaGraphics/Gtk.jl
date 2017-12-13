ad6bab9d20401d3394c659361d080b039db53bc7
Robin Deits <robin.deits@gmail.com>
2017-12-13 16:57:42 -0500

    try adding docker build on travis

---
+++ travis-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+if [[ -a .git/shallow ]]; then 
+	git fetch --unshallow
+fi
+if [[ "$DOCKER_BUILD" = true ]]; then 
+	docker build -t gtkjl . && sudo docker run gtkjl
+else
+	if [[ `uname` = "Linux" ]]; then 
+		TESTCMD="xvfb-run julia"
+	else 
+		TESTCMD="julia"
+	fi
+	$TESTCMD -e 'Pkg.clone(pwd()); using BinDeps;
+	      println(BinDeps.debug("Gtk"));
+	      Pkg.build("Gtk");
+	      Pkg.test("Gtk"; coverage=true)'
+fi
