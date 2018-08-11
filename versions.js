34280e729964dd875f9579b0ba4ff0b5ad920751
autodocs <autodocs>
2018-08-11 19:39:56 +0000

    build based on cc7834b

--- versions.js
+++ versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.16",
   "release-0.14",
   "release-0.13",
   "release-0.12",
   "release-0.11",
+  "v0.16.1",
   "v0.14.0",
   "v0.13.3",
   "v0.13.0",
