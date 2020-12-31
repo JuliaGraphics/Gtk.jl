a202b6ddf4c5ba36cbd953d7e08f22cfe5fb627f
Tim Holy <tim.holy@gmail.com>
2020-12-31 09:35:53 -0600

    Yet more precompiles
    These were identified similarly to #549

--- src/precompile.jl
+++ src/precompile.jl
@@ -3,12 +3,14 @@ function _precompile_()
     precompile(Tuple{typeof(__init__)})
     precompile(Tuple{typeof(gtk_main)})
     precompile(Tuple{Type{GtkWindow},String})
+    precompile(Tuple{Type{GtkWindowLeaf},String,Int,Int})
     precompile(Tuple{Type{GtkMenu}})
     precompile(Tuple{Type{GtkMenuItem},String})
     precompile(Tuple{Type{GtkFrameLeaf},Ptr{GObject}})
     precompile(Tuple{Type{GtkBox},Symbol})
     precompile(Tuple{Type{GtkButtonLeaf},Ptr{GObject}})
     precompile(Tuple{Type{GtkToolButton},String})
+    precompile(Tuple{Type{GtkCanvas},Int,Int})
     precompile(Tuple{typeof(signal_emit),GtkCanvas,String,Type,GdkEventScroll})
     precompile(Tuple{typeof(signal_emit),GtkCanvas,String,Type,GdkEventMotion})
     precompile(Tuple{typeof(signal_emit),GtkCanvas,String,Type,GdkEventButton})
@@ -21,4 +23,39 @@ function _precompile_()
     precompile(Tuple{typeof(notify_motion),Ptr{GObject},Ptr{GdkEventMotion},Gtk_signal_motion{MouseHandler}})   # time: 0.001248276
     precompile(Tuple{typeof(canvas_on_draw_event),Ptr{GObject},Ptr{Nothing},GtkCanvas})   # time: 0.001126875
     precompile(Tuple{typeof(mousedown_cb),Ptr{GObject},Ptr{GdkEventButton},MouseHandler})   # time: 0.001090291
+    precompile(Tuple{typeof(signal_connect),typeof(notify_realize), GtkCanvas, String, Type{Nothing}, Tuple{}})
+    precompile(Tuple{typeof(open_dialog),String,GtkWidget,Tuple{String}})
+    precompile(Tuple{typeof(open_dialog),String,GtkWidget,Tuple{String,String}})
+    precompile(Tuple{typeof(save_dialog),String,GtkWidget,Tuple{String}})
+    precompile(Tuple{typeof(draw),GtkCanvas,Bool})
+    for T in Any[
+        GtkWindowLeaf,
+        GtkMenuLeaf,
+        GtkMenuItemLeaf,
+        GtkFrameLeaf,
+        GtkBoxLeaf,
+        GtkButtonLeaf,
+        GtkToolButtonLeaf,
+        GtkCanvas,
+        GtkWidget,
+        GtkProgressBarLeaf,
+        GtkLabelLeaf,
+        GtkScaleLeaf,
+        GtkBuilderLeaf,
+        GtkEntryLeaf,
+        GtkSpinButtonLeaf,
+        GtkToggleButtonLeaf,
+        GtkCheckButtonLeaf,
+        GtkComboBoxTextLeaf,
+        GtkAspectFrameLeaf,
+        GtkLabel,
+        GtkAdjustmentLeaf,
+        GObject,
+        GtkTextBufferLeaf,
+        GtkTextViewLeaf,
+        GtkTextBuffer,
+        GtkGridLeaf,
+    ]
+        precompile(Tuple{typeof(GLib.gobject_ref), T})
+    end
 end
