99c2721e37e02dadea8a6f8e1cfb19299cb8bf12
coloursofnoise <53288101+coloursofnoise@users.noreply.github.com>
2021-01-01 15:00:07 -0800

    Final formatting fix

--- gen/gtk_auto_gen.jl
+++ gen/gtk_auto_gen.jl
@@ -7,17 +7,17 @@ include("gtk_get_set_gen.jl")
 include("gtk_consts_gen.jl")
 
 function without_linenums!(ex::Expr)
-   linenums_filter(x,ex) = true
-   linenums_filter(x::LineNumberNode,ex) = false
-   linenums_filter(x::Expr,ex) = x.head !== :line
-   linenums_filter(x::Nothing,ex) = ex.head !== :block
-   filter!((x)->linenums_filter(x,ex), ex.args)
-   for arg in ex.args
-      if isa(arg,Expr)
-         without_linenums!(arg)
-      end
-   end
-   ex
+    linenums_filter(x,ex) = true
+    linenums_filter(x::LineNumberNode,ex) = false
+    linenums_filter(x::Expr,ex) = x.head !== :line
+    linenums_filter(x::Nothing,ex) = ex.head !== :block
+    filter!((x)->linenums_filter(x,ex), ex.args)
+    for arg in ex.args
+        if isa(arg,Expr)
+            without_linenums!(arg)
+        end
+    end
+    ex
 end
 
 # If there is an error about missing some std headers, e.g. fatal error: 'time.h' file not found,
@@ -33,61 +33,61 @@ const gtk_h = joinpath(LIBGTK3_INCLUDE, "gtk.h")
 
 toplevels = Any[]
 let gtk_version = 3
-   global trans_unit, root_cursor
-   # parse headers
-   cd(Sys.BINDIR) do
-      global trans_unit = parse_header(gtk_h,
-      args=["-I", joinpath(LIBGTK3_INCLUDE, ".."), "-I$(STD_INCLUDE)"],
-      includes=vcat(LIBGTK3_INCLUDE, CLANG_INCLUDE, DEPENDENCIES, ADDITIONAL),
-      flags=0x41)
-   end
-   
-   root_cursor = getcursor(trans_unit)
-   
-   gboxpath = "gbox$(gtk_version)"
-   gconstspath = "gconsts$(gtk_version)"
-   cachepath = "gtk$(gtk_version)"
-   
-   g_types = gen_g_type_lists(root_cursor)
-   for z in g_types
-      for (s, ex) in z
-         without_linenums!(ex)
-      end
-   end
-   
-   body = Expr(:block,
-   Meta.parse("import ..Gtk"),
-   Meta.parse("import ..Gtk.GObject"),
-   )
-   
-   gbox = Expr(:toplevel,Expr(:module, true, :GAccessor, body))
-   count_fcns = gen_get_set(body, root_cursor)
-   println("Generated $gboxpath with $count_fcns function definitions")
-   without_linenums!(gbox)
-   
-   body = Expr(:block)
-   gconsts = Expr(:toplevel,Expr(:module, true, :GConstants, body))
-   count_consts = gen_consts(body, root_cursor)
-   println("Generated $gconstspath with $count_consts constants")
-   without_linenums!(gconsts)
-   
-   open(joinpath(@__DIR__, gboxpath), "w") do cache
-      Base.println(cache,"quote")
-      Base.show_unquoted(cache, gbox)
-      println(cache)
-      Base.println(cache,"end")
-   end
-   open(joinpath(@__DIR__, gconstspath), "w") do cache
-      Base.println(cache,"quote")
-      Base.show_unquoted(cache, gconsts)
-      println(cache)
-      Base.println(cache,"end")
-   end
-   ser_version = Serialization.ser_version
-   open(joinpath(@__DIR__, "$(cachepath)_julia_ser$(ser_version)"), "w") do cache
-      Serialization.serialize(cache, gbox)
-      Serialization.serialize(cache, gconsts)
-   end
-   push!(toplevels, (gbox, gconsts, g_types, gtk_h))
+    global trans_unit, root_cursor
+    # parse headers
+    cd(Sys.BINDIR) do
+        global trans_unit = parse_header(gtk_h,
+        args=["-I", joinpath(LIBGTK3_INCLUDE, ".."), "-I$(STD_INCLUDE)"],
+        includes=vcat(LIBGTK3_INCLUDE, CLANG_INCLUDE, DEPENDENCIES, ADDITIONAL),
+        flags=0x41)
+    end
+    
+    root_cursor = getcursor(trans_unit)
+    
+    gboxpath = "gbox$(gtk_version)"
+    gconstspath = "gconsts$(gtk_version)"
+    cachepath = "gtk$(gtk_version)"
+    
+    g_types = gen_g_type_lists(root_cursor)
+    for z in g_types
+        for (s, ex) in z
+            without_linenums!(ex)
+        end
+    end
+    
+    body = Expr(:block,
+        Meta.parse("import ..Gtk"),
+        Meta.parse("import ..Gtk.GObject"),
+    )
+    
+    gbox = Expr(:toplevel,Expr(:module, true, :GAccessor, body))
+    count_fcns = gen_get_set(body, root_cursor)
+    println("Generated $gboxpath with $count_fcns function definitions")
+    without_linenums!(gbox)
+    
+    body = Expr(:block)
+    gconsts = Expr(:toplevel,Expr(:module, true, :GConstants, body))
+    count_consts = gen_consts(body, root_cursor)
+    println("Generated $gconstspath with $count_consts constants")
+    without_linenums!(gconsts)
+    
+    open(joinpath(@__DIR__, gboxpath), "w") do cache
+        Base.println(cache,"quote")
+        Base.show_unquoted(cache, gbox)
+        println(cache)
+        Base.println(cache,"end")
+    end
+    open(joinpath(@__DIR__, gconstspath), "w") do cache
+        Base.println(cache,"quote")
+        Base.show_unquoted(cache, gconsts)
+        println(cache)
+        Base.println(cache,"end")
+    end
+    ser_version = Serialization.ser_version
+    open(joinpath(@__DIR__, "$(cachepath)_julia_ser$(ser_version)"), "w") do cache
+        Serialization.serialize(cache, gbox)
+        Serialization.serialize(cache, gconsts)
+    end
+    push!(toplevels, (gbox, gconsts, g_types, gtk_h))
 end
 toplevels
