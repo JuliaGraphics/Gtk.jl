75c6d6f32788c80af475319edcde01d54b5a9e63
Ian <i.r.butterworth@gmail.com>
2022-01-17 18:55:12 -0500

    fix and expand tests

--- test/misc.jl
+++ test/misc.jl
@@ -38,6 +38,13 @@ destroy(win)
 
 @test isa(Gtk.GdkEventKey(), Gtk.GdkEventKey)
 
+# make sure all shown widgets have been destroyed, otherwise the eventloop
+# won't stop automatically
+for (w,_) in Gtk.shown_widgets
+    destroy(w)
+end
+Gtk.wait_eventloop_stopping()
+
 @testset "Eventloop control" begin
     before = Gtk.auto_idle[]
 
@@ -60,6 +67,56 @@ destroy(win)
         @test !Gtk.is_eventloop_running()
     end
     @test Gtk.is_eventloop_running()
+    Gtk.enable_eventloop(false, wait_stopped = true)
+    @test !Gtk.is_eventloop_running()
+
+    @testset "pause_eventloop: multithreaded code doesn't block" begin
+        Gtk.auto_idle[] = true
+        Threads.nthreads() < 1 && @warn "Threads.nthreads() == 1. Multithread blocking tests are not effective"
+
+        function multifoo()
+            Threads.@threads for _ in 1:Threads.nthreads()
+                sleep(0.1)
+            end
+        end
+
+        @test !Gtk.is_eventloop_running()
+        win = Gtk.Window("Multithread test", 400, 300)
+        showall(win)
+        @test Gtk.is_eventloop_running()
+        for i in 1:10
+            Gtk.pause_eventloop() do
+                @test !Gtk.is_eventloop_running()
+                t = @elapsed multifoo() # should take slightly more than 0.1 seconds
+                @test t < 4.5 # given the Glib uv_prepare timeout is 5000 ms
+            end
+        end
+        @test Gtk.is_eventloop_running()
+        destroy(win)
+        Gtk.wait_eventloop_stopping()
+    end
+
+    @testset "wait_eventloop_stopping: waits for stop" begin
+        c = GtkCanvas()
+        win = GtkWindow(c)
+        showall(win)
+        @test Gtk.is_eventloop_running()
+        destroy(win)
+        Gtk.wait_eventloop_stopping()
+        @test !Gtk.is_eventloop_running()
+    end
+
+    @testset "pause_eventloop: doesn't restart a stopping eventloop" begin
+        c = GtkCanvas()
+        win = GtkWindow(c)
+        showall(win)
+        @test Gtk.is_eventloop_running()
+        destroy(win)
+        Gtk.pause_eventloop() do
+            @test !Gtk.is_eventloop_running()
+        end
+        @test !Gtk.is_eventloop_running()
+    end
 
     Gtk.auto_idle[] = before
 end
